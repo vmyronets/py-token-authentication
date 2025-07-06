2d6ed3ed661e8c150d2be0ab17540d9e88830878
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-07-06 19:28:24 +0300

    Remove redundant spaces in cinema/views.py

--- cinema/views.py
+++ cinema/views.py
@@ -109,8 +109,8 @@ class MovieSessionViewSet(
         .select_related("movie", "cinema_hall")
         .annotate(
             tickets_available=F("cinema_hall__rows")
-                              * F("cinema_hall__seats_in_row")
-                              - Count("tickets")
+            * F("cinema_hall__seats_in_row")
+            - Count("tickets")
         )
     )
     serializer_class = MovieSessionSerializer
