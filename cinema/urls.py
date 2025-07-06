bd026b7ae008a96628241cdf28fd17d47dedb3e0
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-07-06 19:47:25 +0300

    Move app_name in cinema and user apps

--- cinema/urls.py
+++ cinema/urls.py
@@ -10,6 +10,8 @@ from cinema.views import (
     OrderViewSet,
 )
 
+app_name = "cinema"
+
 router = routers.DefaultRouter()
 router.register("genres", GenreViewSet)
 router.register("actors", ActorViewSet)
@@ -19,5 +21,3 @@ router.register("movie_sessions", MovieSessionViewSet)
 router.register("orders", OrderViewSet)
 
 urlpatterns = [path("", include(router.urls))]
-
-app_name = "cinema"
--- user/urls.py
+++ user/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 
 from user.views import UserCreateView, CreateTokenView, ManageUserView
 
+
+app_name = "user"
+
 urlpatterns = [
     path("register/", UserCreateView.as_view(), name="create"),
     path("login/", CreateTokenView.as_view(), name="login"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
-
-app_name = "user"
